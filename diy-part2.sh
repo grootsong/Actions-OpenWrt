4b7d9e74dfd38a0d418915f2119cc78dfe85c92a
grootsong <63644470+grootsong@users.noreply.github.com>
2020-06-26 14:36:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,6 +8,6 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP, host name and distribution reversion
+# Modify default IP and distribution reversion
 sed -i 's/192.168.1.1/192.168.16.1/g' package/base-files/files/bin/config_generate
 sed -i 's/R20.6.18/modified by PPS V202006/g' package/lean/default-settings/files/zzz-default-settings
